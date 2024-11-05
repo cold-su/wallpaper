26416bf508c0fb57010e8477059d3613f4b19c05
Yuzh1ruo <coldsu@outlook.com>
2024-11-05 13:42:50 +0800

    todo: 通过另外写一个程序检验是否有序再返回需要排序的目标对象 (#1)

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main() {
+	std::string s;
+	while (std::cin >> s && s[0] != EOF) {
+		// TODO
+	}
+}
--- reorder.sh
+++ reorder.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 # 用于快速重命名全部文件
 
-let i1=1
-for file in $(ls | grep -v reorder.sh | grep -v README); do
-    [ -f $file ] && mv $file $(printf "%09d" $i1).png && let i1=i1+1
-done
+echo $(ls | grep -v reorder.sh | grep -v README) >> temp
 
-let i2=1
+# let i1=1
+# for file in $(ls | grep -v reorder.sh | grep -v README); do
+#     [ -f $file ] && mv $file $(printf "%09d" $i1).png && let i1=i1+1
+# done
 
-for file in $(ls -tr | grep -v reorder.sh | grep -v README); do
-    [ -f $file ] && mv $file $(printf "%03d" $i2).png && let i2=i2+1
-done
+# let i2=1
 
-let sum=i1-1;
+# for file in $(ls -tr | grep -v reorder.sh | grep -v README); do
+#     [ -f $file ] && mv $file $(printf "%03d" $i2).png && let i2=i2+1
+# done
 
-echo $"All done. $sum photos"
+# let sum=i1-1;
 
-echo $'# Wallpapers
+# echo $"All done. $sum photos"
 
-## How to use reorder.sh
+# echo $'# Wallpapers
 
-### On Windows
+# ## How to use reorder.sh
 
-- dep: Git Bash
+# ### On Windows
 
-open Git bash and then:
+# - dep: Git Bash
 
-```bash
-./reorder.sh
-```
+# open Git bash and then:
 
-### On Linux
+# ```bash
+# ./reorder.sh
+# ```
 
-```bash
-./reorder.sh
-```
+# ### On Linux
 
-## Preview\n' > README.md
+# ```bash
+# ./reorder.sh
+# ```
 
-for file in $(ls | grep -v reorder.sh | grep -v README); do
-    echo "$file  " >> README.md
-    echo "![$file]($file)" >> README.md
-done
+# ## Preview\n' > README.md
+
+# for file in $(ls | grep -v reorder.sh | grep -v README); do
+#     echo "$file  " >> README.md
+#     echo "![$file]($file)" >> README.md
+# done
