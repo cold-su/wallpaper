25f09f5918c6a21b2a1b931b376f21c444f4811e
Yuzh1ruo <coldsu@outlook.com>
2024-11-08 13:17:40 +0800

    feat: 现在会处理重名的文件了 (#1)
    重名的文件是指文件名为纯数字的文件。因为它会被认为是经过排序过后的文件，所以需要筛选出来。
    
    hack 1: 对于一个数字巨大的输入，存在 bug

--- main.cpp
+++ main.cpp
@@ -2,43 +2,47 @@
 #define println(...) puts(std::format(__VA_ARGS__).c_str()) // for debugging
 
 // todo: 这些声明到时候放到头文件里面
-std::tuple<bool, std::vector<std::string>, int, int> is_sorted(std::fstream& file);
+std::tuple<bool, std::vector<std::string>, std::map<int, int>, int> get_infos(std::fstream& file);
 
 int main() {
 	std::fstream f;
 	f.open("temp.txt", std::ios::in);
 
-	auto [is, files, pos, n] = is_sorted(f);
-	bool have_unnamed_files = files.size() > 0;
+	auto [is, unnamed_files, same_files, n] = get_infos(f);
 
-	if (have_unnamed_files) {
-		if (is) {
-			std::fstream __f;
-			__f.open("__temp.txt", std::ios::out);
-			for (int i = 0; i < files.size(); i++) {
-				std::string file_name = files[i];
-				__f << std::format("{}.png {}.png\n", file_name, i + n + 1);
-			}
-			__f.close();
-		}
-		// todo
+	std::fstream __f;
+	__f.open("__temp.txt", std::ios::out);
+
+	int i = 0;
+	for (; i < unnamed_files.size(); i++) {
+		std::string file_name = unnamed_files[i];
+		__f << std::format("{}.png {}.png\n", file_name, i + n + 1);
 	}
 
+	int begin = i + n + 1;
+	for (auto [file_name, cnt] : same_files) {
+		while (cnt--) {
+			__f << std::format("{}.png {}.png\n", file_name, begin++);
+		}
+	}
+
+	__f.close();
 	f.close();
 	return 0;
 }
 
 // todo: 拆分这个功能
-std::tuple<bool, std::vector<std::string>, int, int> is_sorted(std::fstream& file) {
-	std::string s;
+std::tuple<bool, std::vector<std::string>, std::map<int, int>, int> get_infos(std::fstream& file) {
 	std::vector<int> vec;
 	std::vector<std::string> unnamed_files;
+
+	std::string s;
 	while (getline(file, s)) {
 		if (s == "") {
 			continue;
 		}
-		std::string new_string;
 
+		std::string new_string;
 		bool named = true;
 		for (char i : s) {
 			if (i == '.') {
@@ -55,22 +59,24 @@ std::tuple<bool, std::vector<std::string>, int, int> is_sorted(std::fstream& fil
 		} else {
 			vec.push_back(std::stoi(new_string) - 1);
 		}
-
 	}
 
-	auto check = [] (std::vector<int> nums) -> std::pair<bool, int> {
+	auto accumulate_same_files = [] (std::vector<int> nums) -> std::map<int, int> {
 		int n = nums.size();
 		int p = 0;
+		int curr = -1;
+
+		std::map<int, int> map;
 		for (int i = 0; i < n; i++) {
 			p += p == nums[i];
+			map[p] += p == curr;
+			curr = p;
 		}
-		p--;
 
-		return {p == nums[n - 1], p};
+		return map;
 	};
 
 	std::ranges::sort(vec);
-	auto [__is_sorted, pos] = check(vec); //todo: pos 是第一个出现的空位
-
-	return { __is_sorted, unnamed_files, pos, vec.size() };
+	bool is_sorted = std::ranges::is_sorted(vec);
+	return { is_sorted, unnamed_files, accumulate_same_files(vec), vec.size() };
 }
